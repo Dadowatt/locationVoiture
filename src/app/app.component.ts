771ee92b1021421d48446b08bd7ca7baa091a5c9
Dado Watt <dadowatt92@gmail.com>
2024-10-29 11:24:18 +0000

    app d'angular de location de voiture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TableVoitureComponent } from './table-voiture/table-voiture.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, SidebarComponent, TableVoitureComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/table-voiture/table-voiture.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-voiture',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-voiture.component.html',
+  styleUrl: './table-voiture.component.scss'
+})
+export class TableVoitureComponent {
+
+  vehicules = [
+    { model: 'Model', marque: 'Toyota', dates: '12/02/22', types: 'Dépannage', voiture: 498 },
+    { model: 'Model', marque: 'Toyota', dates: '12/02/22', types: 'Dépannage', voiture: 503 },
+    { model: 'Model', marque: 'Toyota', dates: '12/02/22', types: 'Dépannage', voiture: 507 }
+  ];
+  
+}
